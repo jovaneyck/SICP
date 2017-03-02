c21d2986eb96713983164cb6c9db88b49f16a01a
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-03-02 17:45:05 +0100

    exercise 1.18: introducing some tail recursion to fast-mult

--- Exercises/Chapter 1/1.17-fast-mult.rkt
+++ Exercises/Chapter 1/1.17-fast-mult.rkt
@@ -11,15 +11,22 @@
 (define (halve n)
   (/ n 2))
 
-(define (even n)
-  (= 0 (remainder n 2)))
-
+;1.17
 (define (fast-mul a b)
   (cond
     ((= 0 b) 0)
     ((= 1 b) a)
-    ((not (even b)) (+ a (fast-mul a (- b 1))))
-    (else (fast-mul (double a) (halve b)))))
+    ((even? b) (fast-mul (double a) (halve b)))
+    (else (+ a (fast-mul a (- b 1))))))
+    
+;1.18
+(define (tailrec-mul a b)
+  (define (rmul a b acc)
+    (cond
+      ((= 0 b) acc)
+      ((even? b) (rmul (double a) (halve b) acc))
+      (else (rmul a (- b 1) (+ acc a)))))
+  (rmul a b 0))
 
 (require rackunit)
 
@@ -40,5 +47,12 @@
 (eq? (fast-mul 3 5) 15)
 (eq? (fast-mul 0 1) 0)
 (eq? (fast-mul 1 0) 0)
+
+(eq? (tailrec-mul 2 4) 8)
+(eq? (tailrec-mul 0 1) 0)
+(eq? (tailrec-mul 1 0) 0)
+(eq? (tailrec-mul 3 5) 15)
+(eq? (tailrec-mul 4 7) 28)
+(eq? (tailrec-mul 3 18) 54)
 
 (print "all done")

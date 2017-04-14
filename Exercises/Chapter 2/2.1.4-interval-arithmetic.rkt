ec5f2a882693542b6c18fff74812491dcc87472c
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-04-14 15:42:34 +0200

    2.11 FINALLY. But learned non-trivial quickcheck in the process. Fun tool to mechanically refactor algorithms

--- Exercises/Chapter 2/2.1.4-interval-arithmetic.rkt
+++ Exercises/Chapter 2/2.1.4-interval-arithmetic.rkt
@@ -8,7 +8,7 @@
   (make-interval (+ (lower-bound x) (lower-bound y))
                  (+ (upper-bound x) (upper-bound y))))
 
-(define (mul-interval x y)
+(define (mul-interval-old x y)
   (let ((p1 (* (lower-bound x) (lower-bound y)))
         (p2 (* (lower-bound x) (upper-bound y)))
         (p3 (* (upper-bound x) (lower-bound y)))
@@ -17,19 +17,34 @@
                    (max p1 p2 p3 p4))))
 
 ;2.11 - let's rework multiplication!
-
-(define (mul-interval-new x y)
+(define (mul-interval x y)
+  (define (pos? n) (or (positive? n) (zero? n)))
+  (define (neg? n) (negative? n))
   (let ((lx (lower-bound x))
         (ux (upper-bound x))
         (ly (lower-bound y))
         (uy (upper-bound y)))
     (cond
-      [(and (positive? lx) (positive? ux) (positive? ly) (positive? uy))
+      [(and (pos? lx) (pos? ux) (pos? ly) (pos? uy))
        (make-interval (* lx ly) (* ux uy))]
-      ;[(and (negative? lx) (negative? ux) (negative? ly) (negative? uy))
-      ;(make-interval (* lx uy) (* ux ly))]
-      [else
-       (mul-interval x y)])))
+      [(and (neg? lx) (neg? ux) (neg? ly) (neg? uy))
+       (make-interval (* ux uy) (* lx ly))]
+      [(and (neg? lx) (neg? ux) (pos? ly) (pos? uy))
+       (make-interval (* lx uy) (* ux ly))]
+      [(and (pos? lx) (pos? ux) (neg? ly) (neg? uy))
+       (make-interval (* ux ly) (* lx uy))]
+      [(and (pos? lx) (pos? ux) (neg? ly) (pos? uy))
+       (make-interval (* ux ly) (* ux uy))]
+      [(and (neg? lx) (pos? ux) (pos? ly) (pos? uy))
+       (make-interval (* lx uy) (* ux uy))]
+      [(and (neg? lx) (neg? ux) (neg? ly) (pos? uy))
+       (make-interval (* lx uy) (* lx ly))]
+      [(and (neg? lx) (pos? ux) (neg? ly) (neg? uy))
+       (make-interval (* ux ly) (* lx ly))]
+      [(and (neg? lx) (pos? ux) (neg? ly) (pos? uy))
+       (make-interval
+        (min (* lx uy) (* ux ly))
+        (max (* lx ly) (* ux uy)))])))
 
 (define (spans-zero i)
   (and
@@ -61,9 +76,9 @@
 
 ;2.8
 (define (sub-interval x y)
-   (add-interval
-    x
-    (make-interval (* -1.0 (upper-bound y)) (* -1.0 (lower-bound y)))))
+  (add-interval
+   x
+   (make-interval (* -1.0 (upper-bound y)) (* -1.0 (lower-bound y)))))
 
 (check-equal?
  (sub-interval
@@ -86,16 +101,16 @@
      (snd (make-interval 3 4)))
   (check-= (width (mul-interval fst snd)) 2.5 0.001))
 (let
-    ((fst (make-interval 1.5 2))
-     (snd (make-interval -1 1)))
+    ((fst (make-interval 1.5 2.0))
+     (snd (make-interval -1.0 1.0)))
   (check-equal? (width (mul-interval fst snd)) 2.0))
 
 (define generator-interval
   (generator-bind
-   (choose-real -100 100)
+   (choose-integer -100 100)
    (λ (lower)
      (generator-bind
-      (choose-real lower 100)
+      (choose-integer lower 100)
       (λ (upper) (generator-unit (make-interval lower upper)))))))
 
 (define sum-of-widths-eq-width-of-sum
@@ -137,18 +152,31 @@
  (mul-interval (make-interval 1 2) (make-interval -3 4))
  (make-interval -6 8))
 (check-equal?
- (mul-interval (make-interval -1 2) (make-interval -3 4))
- (make-interval -6 8))
-(check-equal?
  (mul-interval (make-interval -2 -1) (make-interval -4 -3))
  (make-interval 3 8))
 (check-equal?
  (mul-interval (make-interval -2 -1) (make-interval -3 4))
+ (make-interval -8 6))
+(check-equal?
+ (mul-interval (make-interval 1 2) (make-interval -4 -3))
+ (make-interval -8 -3))
+(check-equal?
+ (mul-interval (make-interval 1 2) (make-interval -3 4))
+ (make-interval -6 8))
+(check-equal?
+ (mul-interval (make-interval -2 1) (make-interval -4 -3))
+ (make-interval -4 8))
+
+(check-equal?
+ (mul-interval (make-interval -1 2) (make-interval -3 4))
+ (make-interval -6 8))
+(check-equal?
+ (mul-interval (make-interval -2 1) (make-interval -3 4))
  (make-interval -8 6))
 
 ;2.11
 (define reworked-multiplication-has-same-result
   (property ([fst generator-interval]
              [snd generator-interval])
-            (equal? (mul-interval fst snd) (mul-interval-new fst snd))))
+            (equal? (mul-interval-old fst snd) (mul-interval fst snd))))
 (check-property reworked-multiplication-has-same-result)

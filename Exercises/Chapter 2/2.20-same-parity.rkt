808304365446d31f14d1d50b4af826afb0fc51c8
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-04-15 18:09:06 +0200

    cleaned up 2.20 with some filter magic

--- Exercises/Chapter 2/2.20-same-parity.rkt
+++ Exercises/Chapter 2/2.20-same-parity.rkt
@@ -1,25 +1,8 @@
 #lang racket
 
-(define (evens l)
-  (if (null? l)
-      null
-      (if (even? (car l))
-          (cons (car l) (evens (cdr l)))
-          (evens (cdr l)))))
-(define (odds l)
-  (if (null? l)
-      null
-      (if (odd? (car l))
-          (cons (car l) (odds (cdr l)))
-          (odds (cdr l)))))
-
-
 (define (same-parity . numbers)
-  (if (null? numbers)
-      null
-      (if (even? (car numbers))
-          (cons (car numbers) (evens (cdr numbers)))
-          (cons (car numbers) (odds (cdr numbers))))))
+  (let ([f (if (even? (car numbers)) even? odd?)])
+    (filter f numbers)))
 
 (require rackunit)
 
@@ -27,3 +10,4 @@
 (check-equal? (same-parity 1 2) (list 1))
 (check-equal? (same-parity 1 2 3) (list 1 3))
 (check-equal? (same-parity 2 1 3 4 7 6) (list 2 4 6))
+(println "Done")

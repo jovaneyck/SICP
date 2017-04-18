134772327cfeccbf5efb35a374a05e126f515d53
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-04-18 21:17:47 +0200

    2.28 flatten lists with fringe

---
+++ Exercises/Chapter 2/2.28-fringe.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require rackunit)
+
+(define ce check-equal?)
+
+(define (fringe tree)
+  (cond
+    [(null? tree) null]
+    [(not (pair? tree)) (list tree)]
+    [else
+     (append
+      (fringe (car tree))
+      (fringe (cdr tree)))]))
+
+(define x (list (list 1 2) (list 3 4)))
+(ce (fringe x) (list 1 2 3 4))
+(ce (fringe (list x x)) (list 1 2 3 4 1 2 3 4))
+(ce (fringe (list (list 1 (list 2 3) 4 (list 5 6)))) (list 1 2 3 4 5 6))
+(println "Done")

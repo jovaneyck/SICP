9ac428a0b2da7077291c86aa7ff8e5f197ebf4f4
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-04-18 22:50:31 +0200

    2.31 tree-map in F#. Because I can :)

---
+++ Exercises/Chapter 2/2.31-tree-map.fsx
@@ -0,0 +1,25 @@
+type Tree =
+    | Leaf of int
+    | Node of Tree list
+
+let square n = pown n 2
+
+let rec treeMap f t =
+    match t with
+    | Leaf value -> Leaf (f value)
+    | Node children -> Node (children |> List.map (treeMap f))
+
+let squareTree t = treeMap square t
+
+#r @"C:\nuget_local\Unquote.3.1.1\lib\net45\Unquote.dll"
+open Swensen.Unquote
+
+let t = 
+    Node([Leaf 1
+          Node([Leaf 2; Node([Leaf 3; Leaf 4]); Leaf 5])
+          Node([Leaf 6; Leaf 7])])
+
+test <@ squareTree t = 
+            Node([Leaf 1
+                  Node([Leaf 4; Node([Leaf 9; Leaf 16]); Leaf 25])
+                  Node([Leaf 36; Leaf 49])]) @>

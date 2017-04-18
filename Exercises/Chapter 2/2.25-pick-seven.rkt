9fdd8427d32cc20d6660782e50d9a97e0f4e1758
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-04-18 21:29:22 +0200

    example of cadadr and variants to shorten car cdr madness

--- Exercises/Chapter 2/2.25-pick-seven.rkt
+++ Exercises/Chapter 2/2.25-pick-seven.rkt
@@ -18,4 +18,11 @@
    (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr l)))))))))))))
  7)
 
+;you can shorten stuff like this (or at leas up until 4 consecutive cars/cdrs using c<adad>r and variants)
+(check-equal?
+ (let (
+       [l (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7))))))])
+   (cadadr (cadadr (cadadr l))))
+ 7)
+
 (println "Done")

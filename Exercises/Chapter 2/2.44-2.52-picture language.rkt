214586abdbd071948bb141a8ad3c7d01376e4553
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-05-07 10:31:41 +0200

    2.47 - 2.49 finally getting stuff on the screen!

--- Exercises/Chapter 2/2.44-2.52-picture language.rkt
+++ Exercises/Chapter 2/2.44-2.52-picture language.rkt
@@ -67,23 +67,119 @@
    (* s (xcor-vect v))
    (* s (ycor-vect v))))
 
-;tests
 (check-equal? (xcor-vect (make-vect 1 2)) 1)
 (check-equal? (ycor-vect (make-vect 1 2)) 2)
 (check-equal? (add-vect (make-vect 1 2) (make-vect 3 4)) (make-vect 4 6))
 (check-equal? (sub-vect (make-vect 4 1) (make-vect 3 2)) (make-vect 1 -1))
 (check-equal? (scale-vect 3 (make-vect 1 4)) (make-vect 3 12))
 
-#|
-;let's actually draw stuff
-(define wave identity)
+;2.47 cons vs. list implementation of lists
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+(define (make-frame-alt origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame f)
+  (car f))
+(define (edge1-frame f)
+  (cadr f))
+(define (edge2-frame f)
+  (caddr f))
+(define (origin-frame-alt f)
+  (car f))
+(define (edge1-frame-alt f)
+  (cadr f))
+(define (edge2-frame-alt f)
+  (cddr f)) ;only difference with the list implementation: last cdr is element instead of list with 1 element
+
+(check-equal? (origin-frame (make-frame 1 2 3)) 1)
+(check-equal? (edge1-frame (make-frame 1 2 3)) 2)
+(check-equal? (edge2-frame (make-frame 1 2 3)) 3)
+(check-equal? (origin-frame-alt (make-frame-alt 1 2 3)) 1)
+(check-equal? (edge1-frame-alt (make-frame-alt 1 2 3)) 2)
+(check-equal? (edge2-frame-alt (make-frame-alt 1 2 3)) 3)
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v)
+                           (edge1-frame frame))
+               (scale-vect (ycor-vect v)
+                           (edge2-frame frame))))))
+
+;2.48
+(define (make-segment start end)
+  (cons start end))
+(define (start-segment segment)
+  (car segment))
+(define (end-segment segment)
+  (cdr segment))
+
+;let's actually draw some stuff!
+(define dimension 100)
+(define target (make-bitmap dimension dimension))
+(define canvas (new bitmap-dc% [bitmap target]))
+(define (draw-line vect-a vect-b)
+  (send canvas draw-line
+        (xcor-vect vect-a)
+        (ycor-vect vect-a)
+        (xcor-vect vect-b)
+        (ycor-vect vect-b)))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+        ((frame-coord-map frame) (start-segment segment))
+        ((frame-coord-map frame) (end-segment segment))))
+     segment-list)))
+
+(println "Done")
+
+(define fullscreen-frame
+  (make-frame (make-vect 0 0) (make-vect 0 dimension) (make-vect dimension 0)))
+
+(define wave 
+  (segments->painter
+   (list
+    (make-segment (make-vect 0 .5) (make-vect .999 .999))
+    (make-segment (make-vect 0 .5) (make-vect .999 0))
+    (make-segment (make-vect .999 0) (make-vect .999 .999))
+    (make-segment (make-vect .999 0.5) (make-vect .5 .25))
+    (make-segment (make-vect .5 .25) (make-vect .5 .75))
+    (make-segment (make-vect .5 .75) (make-vect .999 .5)))))
 (define wave2 (beside wave (flip-vert wave)))
 (define wave4 (flipped-pairs wave))
 
-(define target (make-bitmap 400 400))
-(define canvas (new bitmap-dc% [bitmap target]))
-(send canvas draw-line
-      0 0    ; Start at (0, 0), the top-left corner
-      400 400) ; and draw to the bottom-right corner
+;2.49
+(define outline
+  (segments->painter
+   (list
+    (make-segment (make-vect 0 0) (make-vect 0 .999))
+    (make-segment (make-vect 0 0) (make-vect .999 0))
+    (make-segment (make-vect .999 0) (make-vect .999 .999))
+    (make-segment (make-vect 0 .999) (make-vect .999 .999)))))
 
-(make-object image-snip% target)|#
+(define x
+  (segments->painter
+   (list
+    (make-segment (make-vect 1 0) (make-vect 0 1))
+    (make-segment (make-vect 0 0) (make-vect 1 1)))))
+
+(define diamond
+  (segments->painter
+   (list
+    (make-segment (make-vect 0 .5) (make-vect .5 0))
+    (make-segment (make-vect .5 0) (make-vect 1 .5))
+    (make-segment (make-vect 1 .5) (make-vect .5 1))
+    (make-segment (make-vect .5 1) (make-vect 0 .5)))))
+
+
+(wave fullscreen-frame)
+;(outline fullscreen-frame)
+;(x fullscreen-frame)
+;(diamond fullscreen-frame)
+
+(make-object image-snip% target)

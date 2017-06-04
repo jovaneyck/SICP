9e24a7525e414db40097e27477754b2c5ce1ec4c
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-06-04 21:13:43 +0200

    2.70 Verified encode/decode

--- Exercises/Chapter 2/2.67-decode-huffman.rkt
+++ Exercises/Chapter 2/2.67-decode-huffman.rkt
@@ -138,10 +138,11 @@
   (check-equal? (generate-huffman-tree symbol-frequency-pairs) expected-tree))
 
 ;2.70
-(let ([frequencies '((A 2) (NA 16) (BOOM 1) (SHA 3) (GET 2) (YIP 9) (JOB 2) (WAH 1))]
+(let ([tree (generate-huffman-tree '((A 2) (NA 16) (BOOM 1) (SHA 3) (GET 2) (YIP 9) (JOB 2) (WAH 1)))]
       [message '(GET A JOB SHA NA NA NA NA NA NA NA NA GET A JOB SHA NA NA NA NA NA NA NA NA WAH YIP YIP YIP YIP YIP YIP YIP YIP YIP SHA BOOM)]
       [expected-code '(1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 0 1 1 0 1 1)])
-  (check-equal? (encode message (generate-huffman-tree frequencies)) expected-code))
+  (check-equal? (encode message tree) expected-code)
+  (check-equal? (decode (encode message tree) tree) message))
 ;encoded string: 84 bits
 
 ;fixed length:

e50d75db1dafc990d1938ae99dbd4b39bc88d993
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-06-04 21:00:29 +0200

    2.69 Hoffman create encoding tree

--- Exercises/Chapter 2/2.67-decode-huffman.rkt
+++ Exercises/Chapter 2/2.67-decode-huffman.rkt
@@ -54,26 +54,25 @@
                     [r (right-branch tree)])
                 (cond [(and (leaf? l)
                             (eq? (symbol-leaf l)
-                               symbol))
+                                 symbol))
                        (list 0)]
                       [(and (leaf? r)
                             (eq? (symbol-leaf r)
-                               symbol))
+                                 symbol))
                        (list 1)]
                       [(memq symbol (symbols l))
                        (cons 0 (encode-symbol symbol l))]
                       [(memq symbol (symbols r))
                        (cons 1 (encode-symbol symbol r))]
                       [else (error "symbol not found in tree" symbol tree)]))]))
-                                         
-  
+
+
 
 (define (adjoin-set x set)
   (cond ((null? set) (list x))
         ((< (weight x) (weight (car set))) (cons x set))
         (else (cons (car set)
                     (adjoin-set x (cdr set))))))
-
 
 (define (make-leaf-set pairs)
   (if (null? pairs)
@@ -82,6 +81,18 @@
         (adjoin-set (make-leaf (car pair) ; symbol
                                (cadr pair)) ; frequency
                     (make-leaf-set (cdr pairs))))))
+
+;2.69
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge leaf-set)
+  (if (= 1 (length leaf-set))
+      (car leaf-set)
+      (let ([fst (car leaf-set)]
+            [snd (cadr leaf-set)]
+            [tail (cddr leaf-set)])
+        (successive-merge (adjoin-set (make-code-tree fst snd) tail)))))
 
 (require rackunit)
 ;2.67
@@ -96,5 +107,34 @@
      [raw-message '(A D A B B C A)])
   (check-equal? (decode sample-message sample-tree) raw-message)
   (check-equal? (encode raw-message sample-tree) sample-message))
+
+(let ([symbol-frequency-pairs '((A 4) (B 2) (D 1) (C 1))]
+      [expected-tree '((leaf A 4) ((leaf B 2) ((leaf C 1) (leaf D 1) (C D) 2) (B C D) 4) (A B C D) 8)])
+  (check-equal? (generate-huffman-tree symbol-frequency-pairs) expected-tree))
+
+(let ([symbol-frequency-pairs '((A 8) (B 3) (C 1) (D 1) (E 1) (F 1) (G 1) (H 1))]
+      [expected-tree '((leaf A 8)
+                       ((((leaf H 1)
+                          (leaf G 1)
+                          (H G)
+                          2)
+                         ((leaf F 1)
+                          (leaf E 1)
+                          (F E)
+                          2)
+                         (H G F E)
+                         4)
+                        (((leaf D 1)
+                          (leaf C 1)
+                          (D C)
+                          2)
+                         (leaf B 3)
+                         (D C B)
+                         5)
+                        (H G F E D C B)
+                        9)
+                       (A H G F E D C B)
+                       17)])
+  (check-equal? (generate-huffman-tree symbol-frequency-pairs) expected-tree))
 
 (println "Done")
